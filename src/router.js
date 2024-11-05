e55287294e49db3cbdf2893bc1ef352162f9e14c
fcgalindom <felipecamilo7@gmail.com>
2024-11-04 19:59:37 -0500

    solution haschtag

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
 import customerRoutes from '@/routes/customers.routes'
 import rafflesRoutes from '@/routes/raffle.route'
@@ -20,7 +20,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL), // Cambia a createWebHashHistory
+  history: createWebHistory(import.meta.env.BASE_URL), // Cambiado a createWebHistory para quitar el #
   routes,
 })
 
