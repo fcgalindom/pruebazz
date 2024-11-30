f41c00a2b9b1e344325b303a9a3c289704bd8b7e
Jefferson Linares <ylinares@unbosque.edu.co>
2024-11-30 12:24:47 -0500

    ticket free rifa precargada

--- src/views/charts/chartprincipal.vue
+++ src/views/charts/chartprincipal.vue
@@ -1,9 +1,25 @@
 <template>
-
     <div class="card flex justify-center">
-        <Chart type="pie" :data="chartData" :options="chartOptions" class="w-full md:w-[30rem]" />
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th></th>
+                    <!-- <th>Rifa</th> -->
+                    <th>Cantidad</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(i, index) in tickets" :key="index">
+                    <td>#{{i.number}}</td>
+                    <!-- <td>{{i.raffle.name}}</td> -->
+                    <td>{{ i.customer?.name ?? 'N/A' }}</td>
+                </tr>
+            </tbody>
+        </table>
+        <!-- <Chart type="bar" :data="chartData" :options="chartOptions" class="w-full md:w-[30rem]" /> -->
     </div>
 </template>
+
 <script setup>
 import { ref, onMounted } from "vue";
 import { TicketServices } from "@/services/ticket.service";
@@ -12,15 +28,15 @@ const tickets = ref({});
 const raffles = ref({});
 
 onMounted(async () => {
-    showData(7);
-   
-     chartData.value =  setChartData();
-    chartOptions.value = setChartOptions();
+        showData(7);
 
-} 
-   
-        
-        
+        chartData.value = setChartData();
+        chartOptions.value = setChartOptions();
+
+    }
+
+
+
 );
 
 
@@ -28,33 +44,33 @@ const chartData = ref();
 const chartOptions = ref();
 
 
-const showData = async(id) => {
+const showData = async (id) => {
     tickets.value = await TicketServices.getnumberticket(7);
-        raffles.value = await TicketServices.getnumberrafle(7);
-        console.log("t",tickets.value);
-        console.log("r",raffles.value);
-        
+    console.log('tickets.value', tickets.value);
+    
+    raffles.value = await TicketServices.getnumberrafle(7);
+    console.log("t", tickets.value);
+    console.log("r", raffles.value);
+
 }
 
-const setChartData =  () => {
- 
+const setChartData = () => {
+
     const documentStyle = getComputedStyle(document.body);
-    
-    console.log("t",tickets.value);
-    console.log("r",raffles.value);
+
+    console.log("t", tickets.value);
+    console.log("r", raffles.value);
     return {
         labels: ['Disponibles', 'Pendientes', 'Reservados', 'Pagados'],
-        datasets: [
-            {
-                data: [1,2,1,3],
-                backgroundColor: [documentStyle.getPropertyValue('--p-cyan-500'), documentStyle.getPropertyValue('--p-orange-500'), documentStyle.getPropertyValue('--p-gray-500')],
-                hoverBackgroundColor: [documentStyle.getPropertyValue('--p-cyan-400'), documentStyle.getPropertyValue('--p-orange-400'), documentStyle.getPropertyValue('--p-gray-400')]
-            }
-        ]
+        datasets: [{
+            data: [1, 2, 1, 3],
+            backgroundColor: [documentStyle.getPropertyValue('--p-cyan-500'), documentStyle.getPropertyValue('--p-orange-500'), documentStyle.getPropertyValue('--p-gray-500')],
+            hoverBackgroundColor: [documentStyle.getPropertyValue('--p-cyan-400'), documentStyle.getPropertyValue('--p-orange-400'), documentStyle.getPropertyValue('--p-gray-400')]
+        }]
     };
 };
 
-const setChartOptions =  () => {
+const setChartOptions = () => {
     const documentStyle = getComputedStyle(document.documentElement);
     const textColor = documentStyle.getPropertyValue('--p-text-color');
 
